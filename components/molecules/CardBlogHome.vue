9b0c03f63bbde3f2fa36744e249e7e070b91b657
ngxson4160 <ng.xson194160@gmail.com>
2024-06-28 23:47:17 +0700

    fix UI home

--- components/molecules/CardBlogHome.vue
+++ components/molecules/CardBlogHome.vue
@@ -1,27 +1,41 @@
 <template>
-  <div class="w-[434px] h-[588px] bg-white border p-4 rounded-lg">
-    <img
-      src="@/assets/images/logo.jpg"
-      class="w-full h-[255px] object-contain border rounded-md"
-    />
-    <p class="my-2">18/04/2024</p>
-    <p class="text-lg font-bold mb-4 max-h-14 line-clamp-2">
-      fdjkas sajdk asjidh auisdfgh iuasfgh iuasgfu sadufk asiufyas fduiasdhgf
-      asdiu
-    </p>
-    <p class="text-gray text-sm max-h-[100px] overflow-hidden">
-      fdjkas sajdk asjidh auisdfgh iuasfgh iuasgfu sadufk asiufyas fduiasdhgf
-      asdiu fdjkas sajdk asjidh auisdfgh iuasfgh iuasgfu sadufk asiufyas
-      fduiasdhgf asdiu fdjkas sajdk asjidh auisdfgh iuasfgh iuasgfu sadufk
-      asiufyas fduiasdhgf asdiufdjkas sajdk asjidh auisdfgh iuasfgh iuasgfu
-    </p>
-    <el-button type="primary" class="mt-6 !h-11 !font-bold !text-base">
+  <div
+    class="w-[434px] h-[588px] bg-white border p-4 rounded-lg flex flex-col justify-between"
+  >
+    <div>
+      <img
+        :src="data?.image"
+        class="w-full h-[255px] object-contain border rounded-md"
+      />
+      <p class="my-2">{{ formatDateShort(data?.createdAt) }}</p>
+      <p class="text-lg font-bold mb-4 max-h-14 line-clamp-2">
+        {{ data?.title }}
+      </p>
+
+      <div class="text-gray text-sm max-h-[125px] overflow-hidden">
+        <div v-html="data?.content"></div>
+      </div>
+    </div>
+    <el-button
+      type="primary"
+      class="mt-6 !h-11 !font-bold !text-base"
+      @click="router.push(`/blog/${data?.id}`)"
+    >
       Chi tiết
       <img src="@/assets/images/arrow-right.svg" class="w-8 ml-1" />
     </el-button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps({
+  data: {
+    type: Object,
+    required: true,
+  },
+});
+
+const router = useRouter();
+</script>
 
 <style></style>
